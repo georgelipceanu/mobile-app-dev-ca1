68c5a01e2422321889a197667a429feab32f23a5
georgelipceanu <george.lipceanu@gmail.com>
2025-10-15 23:03:50 +0100

    Add Cloud Job button with Event Handling

--- app/src/main/java/com/example/ca1/CloudJobActivity.kt
+++ app/src/main/java/com/example/ca1/CloudJobActivity.kt
@@ -1,18 +1,34 @@
 package com.example.ca1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.ca1.databinding.ActivityCloudjobBinding
+import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
+import timber.log.Timber.i
 
 class CloudJobActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCloudjobBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cloudjob)
+        binding = ActivityCloudjobBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
-        Timber.i("CloudJob Activity started..")
+        i("CloudJob Activity started..")
+
+        binding.btnAdd.setOnClickListener() {
+            val cloudjobTitle = binding.cloudjobTitle.text.toString()
+            if (cloudjobTitle.isNotEmpty()) {
+                i("add Button Pressed: $cloudjobTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+        setContentView(binding.root)
     }
 }
