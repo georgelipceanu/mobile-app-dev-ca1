a2a22f5a799e443252d581f8cde046ef212642f4
georgelipceanu <george.lipceanu@gmail.com>
2025-10-15 22:45:37 +0100

    Update Layouts

--- app/src/main/java/com/example/ca1/CloudJobActivity.kt
+++ app/src/main/java/com/example/ca1/CloudJobActivity.kt
@@ -10,13 +10,8 @@ import timber.log.Timber
 class CloudJobActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_cloudjob)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         Timber.plant(Timber.DebugTree())
         Timber.i("CloudJob Activity started..")
     }
