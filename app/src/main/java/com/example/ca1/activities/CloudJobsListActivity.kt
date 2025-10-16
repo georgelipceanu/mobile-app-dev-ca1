e7c17e2807255bb7ef4bfee454e8411f5aab787d
georgelipceanu <george.lipceanu@gmail.com>
2025-10-16 23:31:11 +0100

    Refactor adapters into their own directory (Exercise 3, LAB-A02 COMPLETE)

--- app/src/main/java/com/example/ca1/activities/CloudJobsListActivity.kt
+++ app/src/main/java/com/example/ca1/activities/CloudJobsListActivity.kt
@@ -1,21 +1,16 @@
 package com.example.ca1.activities
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.ca1.R
+import com.example.ca1.adapters.CloudJobAdapter
 import com.example.ca1.databinding.ActivityCloudJobsListBinding
-import com.example.ca1.databinding.CardCloudJobBinding
 import com.example.ca1.main.MainApp
-import com.example.ca1.models.CloudJobModel
 
 class CloudJobsListActivity : AppCompatActivity() {
     lateinit var app: MainApp
@@ -58,31 +53,4 @@ class CloudJobsListActivity : AppCompatActivity() {
                 notifyItemRangeChanged(0,app.cloudJobs.size)
             }
         }
-}
-
-class CloudJobAdapter (private var cloudJobs: List<CloudJobModel>) :
-    RecyclerView.Adapter<CloudJobAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardCloudJobBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val cloudJob = cloudJobs[holder.adapterPosition]
-        holder.bind(cloudJob)
-    }
-
-    override fun getItemCount(): Int = cloudJobs.size
-
-    class MainHolder(private val binding : CardCloudJobBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(cloudJob: CloudJobModel) {
-            binding.cloudjobTitle.text = cloudJob.title
-            binding.description.text = cloudJob.description
-        }
-    }
 }
