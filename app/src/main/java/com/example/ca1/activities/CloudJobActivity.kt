97c6143cd2886f4650ae8000abdc2a43d5c6c5eb
georgelipceanu <george.lipceanu@gmail.com>
2025-10-16 20:42:10 +0100

    Add and Integrate MainApp in project

--- app/src/main/java/com/example/ca1/activities/CloudJobActivity.kt
+++ app/src/main/java/com/example/ca1/activities/CloudJobActivity.kt
@@ -3,31 +3,29 @@ package com.example.ca1.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ca1.databinding.ActivityCloudjobBinding
+import com.example.ca1.main.MainApp
 import com.example.ca1.models.CloudJobModel
 import com.google.android.material.snackbar.Snackbar
-import timber.log.Timber
 import timber.log.Timber.i
 class CloudJobActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCloudjobBinding
     var cloudJob = CloudJobModel()
-    val cloudJobs = ArrayList<CloudJobModel>()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCloudjobBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Timber.plant(Timber.DebugTree())
-        i("CloudJob Activity started..")
-
+        app = application as MainApp
         binding.btnAdd.setOnClickListener() {
             cloudJob.title = binding.cloudjobTitle.text.toString()
             cloudJob.description = binding.description.text.toString()
             if (cloudJob.title.isNotEmpty()) {
-                cloudJobs.add(cloudJob.copy())
+                app.cloudJobs.add(cloudJob.copy())
                 i("add Button Pressed: $cloudJob.title")
-                for (i in cloudJobs.indices)
-                    { i("Placemark[$i]:${this.cloudJobs[i]}") }
+                for (i in app.cloudJobs.indices)
+                { i("Placemark[$i]:${this.app.cloudJobs[i]}") }
             }
             else {
                 Snackbar
