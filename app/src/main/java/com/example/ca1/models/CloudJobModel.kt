2d27f014aa9ca546a46c41c4cd9be291e18f66da
georgelipceanu <george.lipceanu@gmail.com>
2025-10-17 23:03:29 +0100

    Parcelize CloudJobModel

--- app/src/main/java/com/example/ca1/models/CloudJobModel.kt
+++ app/src/main/java/com/example/ca1/models/CloudJobModel.kt
@@ -1,4 +1,8 @@
 package com.example.ca1.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CloudJobModel(var title: String = "",
-                         var description: String = "")
+                         var description: String = "") : Parcelable
