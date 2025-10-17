eecff30d5ef7bb77bc608c36ac240ad697b56c65
georgelipceanu <george.lipceanu@gmail.com>
2025-10-17 23:24:19 +0100

    Add update functionality (Exercise 3, LAB-A03 COMPLETED)

--- app/src/main/java/com/example/ca1/activities/CloudJobActivity.kt
+++ app/src/main/java/com/example/ca1/activities/CloudJobActivity.kt
@@ -13,6 +13,7 @@ import timber.log.Timber.i
 class CloudJobActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCloudjobBinding
     var cloudJob = CloudJobModel()
+    var edit = false
     lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,7 @@ class CloudJobActivity : AppCompatActivity() {
         app = application as MainApp
 
         if (intent.hasExtra("cloud_job_edit")) {
+            edit = true
             cloudJob = intent.extras?.getParcelable("cloud_job_edit")!!
             binding.cloudjobTitle.setText(cloudJob.title)
             binding.description.setText(cloudJob.description)
@@ -35,15 +37,17 @@ class CloudJobActivity : AppCompatActivity() {
         binding.btnAdd.setOnClickListener() {
             cloudJob.title = binding.cloudjobTitle.text.toString()
             cloudJob.description = binding.description.text.toString()
-            if (cloudJob.title.isNotEmpty()) {
-                app.cloudJobs.create(cloudJob.copy())
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
+            if (cloudJob.title.isEmpty()) {
                 Snackbar.make(it,R.string.enter_cloud_job_title, Snackbar.LENGTH_LONG).show()
+            } else {
+                if (edit) {
+                    app.cloudJobs.update(cloudJob.copy())
+                } else {
+                    app.cloudJobs.create(cloudJob.copy())
+                }
             }
-
+            setResult(RESULT_OK)
+            finish()
         }
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
