2d9799a94c631ba8ad295a1098437d4c0b791cd0
georgelipceanu <george.lipceanu@gmail.com>
2025-12-10 16:45:27 +0000

    Simplify CloudJobMapsActivity

--- app/src/main/java/com/example/ca1/activities/CloudJobMapsActivity.kt
+++ app/src/main/java/com/example/ca1/activities/CloudJobMapsActivity.kt
@@ -11,8 +11,6 @@ import com.example.ca1.R
 import com.example.ca1.databinding.ActivityCloudJobMapsBinding
 
 class CloudJobMapsActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityCloudJobMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +20,5 @@ class CloudJobMapsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_cloud_job_maps)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_cloud_job_maps)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
