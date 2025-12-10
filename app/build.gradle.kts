c496ebc459a1e479361a39626286f8e868637024
georgelipceanu <george.lipceanu@gmail.com>
2025-12-10 17:17:17 +0000

    Content Layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/ca1/activities/CloudJobMapsActivity.kt
+++ app/src/main/java/com/example/ca1/activities/CloudJobMapsActivity.kt
@@ -9,9 +9,17 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.ca1.R
 import com.example.ca1.databinding.ActivityCloudJobMapsBinding
+import com.example.ca1.databinding.ContentCloudJobMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class CloudJobMapsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCloudJobMapsBinding
+    private lateinit var contentBinding: ContentCloudJobMapsBinding
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +28,32 @@ class CloudJobMapsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        contentBinding = ContentCloudJobMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
     }
 }
