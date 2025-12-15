f2a388ec164b9a27a33731b52ec3af8e9ae90f72
georgelipceanu <george.lipceanu@gmail.com>
2025-12-15 18:17:02 +0000

    CloudJobMap Activity to Presenter/View

---
+++ app/src/main/java/com/example/ca1/views/map/CloudJobMapView.kt
@@ -0,0 +1,73 @@
+package com.example.ca1.views.map
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.squareup.picasso.Picasso
+import com.example.ca1.databinding.ActivityCloudJobMapsBinding
+import com.example.ca1.databinding.ContentCloudJobMapsBinding
+import com.example.ca1.main.MainApp
+import com.example.ca1.models.CloudJobModel
+
+class CloudJobMapView : AppCompatActivity() , GoogleMap.OnMarkerClickListener{
+
+    private lateinit var binding: ActivityCloudJobMapsBinding
+    private lateinit var contentBinding: ContentCloudJobMapsBinding
+    lateinit var app: MainApp
+    lateinit var presenter: CloudJobMapPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = application as MainApp
+        binding = ActivityCloudJobMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        presenter = CloudJobMapPresenter(this)
+
+        contentBinding = ContentCloudJobMapsBinding.bind(binding.root)
+
+        contentBinding.mapView.onCreate(savedInstanceState)
+        contentBinding.mapView.getMapAsync{
+            presenter.doPopulateMap(it)
+        }
+    }
+    fun showCloudJob(cloudJob: CloudJobModel) {
+        contentBinding.currentTitle.text = cloudJob.title
+        contentBinding.currentDescription.text = cloudJob.description
+        Picasso.get()
+            .load(cloudJob.image)
+            .into(contentBinding.imageView)
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        presenter.doMarkerSelected(marker)
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+}
