00d37084162456e323a58e9fe43f494a3d2b8773
georgelipceanu <george.lipceanu@gmail.com>
2025-12-16 21:28:42 +0000

    Email sign in and sign up added

---
+++ app/src/main/java/com/example/ca1/views/auth/AuthView.kt
@@ -0,0 +1,45 @@
+package com.example.ca1.views.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ca1.databinding.ActivityAuthBinding
+import com.example.ca1.views.cloudjoblist.CloudJobListView
+import com.google.android.material.snackbar.Snackbar
+
+class AuthView : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+    private lateinit var presenter: AuthPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        presenter = AuthPresenter(this)
+
+        binding.btnSignIn.setOnClickListener {
+            presenter.doSignIn(
+                binding.emailField.text.toString(),
+                binding.passwordField.text.toString()
+            )
+        }
+        binding.btnSignUp.setOnClickListener {
+            presenter.doSignUp(
+                binding.emailField.text.toString(),
+                binding.passwordField.text.toString()
+            )
+        }
+    }
+
+    fun showError(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun navigateToApp() {
+        startActivity(Intent(this, CloudJobListView::class.java))
+        finish()
+    }
+}
+
